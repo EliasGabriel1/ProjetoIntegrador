107b7bc14365276522af51664fcce9b71cd28311
VictorAbrao <vi-abrao@hotmail.com>
2021-05-06 20:50:25 -0300

    Correção de Bugs

---
+++ Content/JavaScript/FormularioUsuario.js
@@ -0,0 +1,7 @@
+function perguntaquatro(){
+    document.getElementById("outros").style.display= "block";
+}
+
+function perguntaquatroinvisivel(){
+    document.getElementById("outros").style.display= "none";
+}
